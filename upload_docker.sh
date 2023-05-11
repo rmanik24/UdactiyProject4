c8c23392fedbf6987b7573f559a7b87332adf11f
EC2 Default User <ec2-user@ip-172-31-4-221.ec2.internal>
2023-05-11 19:03:03 +0000

    commiting till task 5 upload image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="rmanik24/demolocal"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u rmanik24
+docker image tag demolocal $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
