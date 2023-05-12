457d743533a5c1c278e546fb7df8858425ca3d39
EC2 Default User <ec2-user@ip-172-31-19-183.ec2.internal>
2023-05-12 08:53:02 +0000

    adding kubernetes and output logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="rmanik24/demolocal"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run demolocal\
+    --image=$dockerpath\
+    --port=80 --labels app=demolocal
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward demolocal 8000:80
 
